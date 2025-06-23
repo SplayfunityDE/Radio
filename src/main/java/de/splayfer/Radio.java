d6eb33eebe10e61bfb8ededf332180496222b04e
Splayfer <splayfery@gmail.com>
2025-06-23 16:33:06 +0200

    Remove dotenv import call

--- src/main/java/de/splayfer/Radio.java
+++ src/main/java/de/splayfer/Radio.java
@@ -6,7 +6,6 @@ import com.sedmelluq.discord.lavaplayer.source.AudioSourceManagers;
 import de.splayfer.radio.RadioManager;
 import de.splayfer.radio.PlayerManager;
 import de.splayfer.utils.CommandManager;
-import io.github.cdimascio.dotenv.Dotenv;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.OnlineStatus;
